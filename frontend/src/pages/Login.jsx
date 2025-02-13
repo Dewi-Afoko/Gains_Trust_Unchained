e18126fa65c1149a4a79fd3ca15e6efa8b9fa472
Dewi Afoko <dewi.afoko@gmail.com>
2025-02-13 17:26:05 +0000

    updated login returns for login component, success/failure messages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import LoginForm from '../components/forms/LoginForm'
 
 const Login = ({ setIsLoggedIn }) => {
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center h-screen bg-[#8B0000]">
             <LoginForm setIsLoggedIn={setIsLoggedIn} />
         </div>
     )
