b88e081b45cdde6247c7d079429c41fcb69162cb
Dewi Afoko <dewi.afoko@gmail.com>
2025-02-17 15:22:15 +0000

    found a workaround for last_login to make it function with JWTs, user model has a login array capped at 2, updates last_login based on the first value in the list

--- Gains_Trust/users/signals.py
+++ Gains_Trust/users/signals.py
@@ -1,14 +1,10 @@
 from django.contrib.auth.signals import user_logged_in
-from django.utils.timezone import now
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 @receiver(user_logged_in)
-def store_previous_last_login(sender, request, user, **kwargs):
-    """Store the previous last_login before it gets updated"""
-    if user.last_login:
-        user.previous_last_login = user.last_login  # Store the previous login
-    user.last_login = now()  # Update last_login as usual
-    user.save(update_fields=["previous_last_login", "last_login"])
+def update_login_history(sender, request, user, **kwargs):
+    """✅ Runs every time a user logs in and updates login history."""
+    user.track_login()  # Calls custom method using list to return the previous login, not this one.
