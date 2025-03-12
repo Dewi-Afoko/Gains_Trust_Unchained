7f2105eade0c6f9c8fcc24ea8e487d6d7120c7b5
Dewi Afoko <dewi.afoko@gmail.com>
2025-03-12 20:41:01 +0000

    finished ViewSet actions for SetDicts because I'm a nerd

--- Gains_Trust/workouts/urls.py
+++ Gains_Trust/workouts/urls.py
@@ -3,8 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     WorkoutViewSet,
     SetDictViewSet,
-    skip_set,
-    move_set,
 )
 
 # 🚀 DRF Router API Endpoints
@@ -14,6 +12,4 @@ router.register(r'sets', SetDictViewSet, basename="sets")
 
 urlpatterns = [
     path("", include(router.urls)),  # ✅ Registers all workout routes automatically
-    path("<int:workout_id>/sets/<int:set_dict_id>/skip/", skip_set, name="set-skip"),
-    path("<int:workout_id>/sets/<int:set_dict_id>/move/", move_set, name="set-move"),
 ]
