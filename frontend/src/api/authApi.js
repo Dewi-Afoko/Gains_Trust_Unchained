1bd85d5bbda25a6275d6545eee9a23f128a10cdc
Dewi Afoko <dewi.afoko@gmail.com>
2025-05-02 10:56:43 +0100

    apis centralised for rebuild

---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,21 @@
+import apiClient from './apiClient';
+
+export const login = async (username, password) => {
+  const response = await apiClient.post('/users/login/', { username, password });
+  return response.data;
+};
+
+export const register = async (userData) => {
+  const response = await apiClient.post('/users/register/', userData);
+  return response.data;
+};
+
+export const logout = async (refreshToken) => {
+  const response = await apiClient.post('/users/logout/', { refresh: refreshToken });
+  return response.data;
+};
+
+export const refreshToken = async (refreshToken) => {
+  const response = await apiClient.post('/users/token/refresh/', { refresh: refreshToken });
+  return response.data;
+};
