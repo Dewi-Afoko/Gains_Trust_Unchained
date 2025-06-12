f4cdd2c946f1c12dc316681fda95b04eb72822bf
Dewi Afoko <dewi.afoko@gmail.com>
2025-06-12 12:14:56 +0100

    fixing dependencies

--- frontend/src/stores/authStore.js
+++ frontend/src/stores/authStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import axios from 'axios'
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000'
 
 const useAuthStore = create((set, get) => ({
     user: null,
