19e8aec6742abd6f20652a4f2eb60613160a6687
Dewi Afoko <dewi.afoko@gmail.com>
2025-06-13 09:11:13 +0100

    Fix favicon serving: add explicit URL routes before catch-all to serve favicon files properly

--- Gains_Trust/Gains_Trust/urls.py
+++ Gains_Trust/Gains_Trust/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
+import os
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -35,6 +38,14 @@ urlpatterns = [
     # DRF Spectacular doc generation
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    
+    # Favicon and icon files - MUST come before catch-all
+    path("favicon.ico", serve, {"document_root": settings.REACT_APP_DIR, "path": "favicon.ico"}),
+    path("favicon-16.png", serve, {"document_root": settings.REACT_APP_DIR, "path": "favicon-16.png"}),
+    path("favicon-32.png", serve, {"document_root": settings.REACT_APP_DIR, "path": "favicon-32.png"}),
+    path("logo192.png", serve, {"document_root": settings.REACT_APP_DIR, "path": "logo192.png"}),
+    path("logo512.png", serve, {"document_root": settings.REACT_APP_DIR, "path": "logo512.png"}),
+    path("manifest.json", serve, {"document_root": settings.REACT_APP_DIR, "path": "manifest.json"}),
     
     # React Frontend - Catch all other routes
     re_path(r'^.*$', ReactAppView.as_view(), name='react_app'),
